e422224fdba92c0019599538566d4029f3d9d347
Cali757 <cali757@example.com>
2025-12-19 19:56:46 -0600

    Fix firebaseAdmin to use environment variables for Vercel

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -2,8 +2,15 @@
 import admin from "firebase-admin";
 
 if (!admin.apps.length) {
+  const serviceAccount = process.env.GOOGLE_APPLICATION_CREDENTIALS
+    ? JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS)
+    : {
+        client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+        private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      };
+
   admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
+    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
   });
 }
 
